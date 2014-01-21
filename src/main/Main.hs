d4b78294b191e4ff88292a8ca413599eb7bc44e3
Chris Done <chrisdone@gmail.com>
2014-01-21 14:03:43 +0100

    Remove redundant imports

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -7,24 +7,8 @@
 
 module Main where
 
-
-import           DataAnalysis.Application.Prelude
-import           DataAnalysis.Application.Types
-import           DataAnalysis.Application.Types.Stock
-
-
-import           Control.Applicative
-import           Control.Lens
-import           Data.Conduit
-import qualified Data.Conduit.Analysis                as CA
-import qualified Data.Conduit.List                    as CL
-import           Data.Default
-import           Data.Proxy
-import           Data.Text                            (pack)
-import qualified Data.Vector                          as V
-import           UserAnalysis
-import           UserParameters
-import           Yesod
+import DataAnalysis.Application.Prelude
+import UserAnalysis
 
 -- | Start the analysis server with the following configuration.
 main :: IO ()
