36b37b70dd1415fd30da67c685444600fc9aa55d
Chris Done <chrisdone@gmail.com>
2014-01-28 16:44:51 +0100

    Change export to use datapoints as source rather than original file

--- src/DataAnalysis/Application/Handler/Export.hs
+++ src/DataAnalysis/Application/Handler/Export.hs
@@ -7,25 +7,32 @@
 module DataAnalysis.Application.Handler.Export where
 
 import           Blaze.ByteString.Builder.ByteString
+import           Data.CSV.Conduit
 import           Data.Conduit
-import           Data.Conduit.Binary
+
 import qualified Data.Conduit.List as CL
+import           Data.Default
 import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Yesod
 
 import           DataAnalysis.Application.Foundation
+import           DataAnalysis.Application.Analyze
 import           DataAnalysis.Application.Types
 
 -- | Export the data source to various data formats.
 getExportR :: Text -> Handler TypedContent
 getExportR ident = do
-    fp <- getById ident
     addHeader "content-disposition" $ T.concat
         [ "attachment; filename=\""
         , T.pack (show ident) <> "-export"
         , ".csv\""
         ]
+    source <- analysisSource ident
     respondSource "text/csv"
-                  (sourceFile (srcPath fp) $= CL.map (Chunk . fromByteString))
+                  (source
+                   $= CL.map toMapRow
+                   $= (writeHeaders settings >> fromCSV settings)
+                   $= CL.map (Chunk . fromByteString))
+  where settings = def
