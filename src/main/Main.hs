6c5ffcbc1aecb568e3f13b25b8d16d1eb503e322
Chris Done <chrisdone@gmail.com>
2014-02-06 14:20:12 +0100

    Add back Main.hs
    @manny-fp I removed PharmaMain.hs file when I converted the Pharma
    example thinking it was unused, seems it was. So I've added it back as
    Main.hs and updated the script.

---
+++ src/main/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Main entry point to my analysis app.
+
+module Main where
+
+import DataAnalysis.Application.Prelude
+import UserAnalysis
+
+-- | Start the analysis server with the following configuration.
+main :: IO ()
+main = runAnalysisApp "Analysis" userAnalysis
